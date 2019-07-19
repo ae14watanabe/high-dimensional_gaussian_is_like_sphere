d3f1fc66ecb51898a74dd4ed719985cd731ae02a
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2019-07-19 12:06:48 +0900

    Code function which outputs histgrams and ploar figure

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n_samples = 10000
-max_dimension = 1000
-step = 100
-dimension_list = np.arange(0,max_dimension,step)
-dimension_list[0] = 1
-n_dimension = len(dimension_list)
-
-seed = 100
-np.random.seed(seed)
-
-fig = plt.figure(figsize=[15,3*(max_dimension//5)])
-
-for i,dim in enumerate(dimension_list):
-    mean = np.zeros(dim)
-    cov = np.identity(dim)
+def make_figures(n_dimension=2, n_samples=1000000):
+    mean = np.zeros(n_dimension)
+    cov = np.identity(n_dimension)
     x = np.random.multivariate_normal(mean,cov,size=n_samples)
-    x_norm = np.sqrt(np.sum(np.square(x),axis=1))
-    ax = fig.add_subplot(n_dimension // 5, 5, i+1)
-    ax.set_title('dim={}\nanalytical mean={}'.format(dim,np.sqrt(2)))
-    ax.hist(x_norm,bins=50,range=(0,50))
+    # calculate r, norm of x
+    r = np.sqrt(np.sum(np.square(x),axis=1))
 
-plt.show()
+    # calculate theta, degree of x
+    e = np.zeros(n_dimension) # unit vector
+    e[0] = 1.0
+    inner_products = np.einsum("d,nd->n",e,x)
+    theta = np.arccos(inner_products / r) # 0 ~ pi
+    # theta *= np.random.choice([1.0,-1.0],n_samples) # -pi ~ pi
+
+    fig = plt.figure(figsize=[10,5])
+    fig.suptitle("{} dimension".format(n_dimension))
+    ax_hist_norm = fig.add_subplot(131)
+    ax_hist_theta = fig.add_subplot(132)
+    ax_polar = fig.add_subplot(133,aspect='equal')
+
+    ax_polar.scatter(r * np.cos(theta), r * np.sin(theta))
+    ax_polar.set_title("$(r\cos \theta,r\sin \theta)$")
+
+    ax_hist_norm.hist(r,bins=50,range=(0,50))
+    ax_hist_norm.set_title("histgram of norm $r$")
+
+    ax_hist_theta.hist(theta,bins=50)
+    ax_hist_theta.set_title("histgram of angle $\theta$")
+
+    plt.show()
+
+if __name__ == '__main__':
+    seed = 100
+    np.random.seed(seed)
+
+    make_figures(2)
+    list_n_dimension = [2,3,4]
+    # for n_dimension in list_n_dimension:
+    #     pass
+
